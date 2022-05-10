4a547c539ad15dd7431f2878916fe4d287f0af36
KimNamWook <7922378@naver.com>
2022-05-10 13:34:24 +0900

    해결

--- main.cpp
+++ main.cpp
@@ -4,65 +4,63 @@ using namespace std;
 
 int Sumf(int* Array, int Count)
 {
-	int Sum = 0;
+    int Sum = 0;
 
-	for (int i = 0; i < Count; ++i)
-	{
-		Sum += Array[i];
-	}
+    for (int i = 0; i < Count; ++i)
+    {
+        Sum += Array[i];
+    }
     return Sum;
 }
-
-int Arrange(int* Array, int Count)
+void Arrange(int* Array, int Count)
 {
     for (int i = 1; i < Count; i++)
     {
         int j = 0;
 
-        for (j = i-1; j < Count - 1  ; j++)
+        for (j = i - 1; j >=0; j--)
         {
-            if (Array[j] > Array[j + 1])
+            if (Array[j] > Array[i])
             {
-                int Temp = 0;
-                Temp = Array[j + 1];
                 Array[j + 1] = Array[j];
-                Array[j] = Temp;
             }
             else
             {
-                Array[j] = Array[j];
+                break;
             }
         }
+        Array[j + 1] = Array[i];
     }
     for (int i = 0; i < Count; i++)
     {
         cout << Array[i] << endl;
     }
-    return 0;
 }
 
 int main()
 {
     int Count = 0;
 
-	cin >> Count;
-	
+    cin >> Count;
+
     int* Array = nullptr;
-    
+
     Array = new int[Count];
 
-	for (int i = 0; i < Count; i++)
-	{
-		cin >> Array[i]; 
-	}
+    for (int i = 0; i < Count; i++)
+    {
+        cin >> Array[i];
+    }
 
     cout << "위 숫자들의 합은 " << Sumf(Array, Count) << endl;
-    cout << "               " << endl;
+    cout << "                 " << endl;
     cout << "위 숫자들을 정렬해봅시다." << endl;
-    cout << Arrange(Array, Count) << endl;
+
+    Arrange(Array, Count);
 
     delete[] Array;
+
     Array = nullptr;
 
-	return 0;
+    return 0;
 }
